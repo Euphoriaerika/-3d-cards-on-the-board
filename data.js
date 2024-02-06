ac54d3188e718f920a772f57ea1986b320c8cd7b
Bohdan Rudenko <b.r.rudenko@student.khai.edu>
2024-02-06 17:50:22 +0200

    feat: added new feature for generating n cards with random predictions

---
+++ data.js
@@ -0,0 +1,13 @@
+var cardData = {
+  "card-1":
+    "Embrace the uncertainty ahead, for within this chaos may lie the key to new opportunities.",
+  "card-2":
+    "Preserve your resources and brace yourself for potential financial challenges that may arise in the near future.",
+  "card-3":
+    "The winds of change are whispering around you; stay attuned to the subtle shifts, as they may guide you towards unexpected transformations.",
+  "card-4":
+    "The winds of change are whispering around you; stay attuned to the subtle shifts, as they may guide you towards unexpected transformations.",
+};
+
+// Викликайте функцію з app.js, яка буде створювати та додавати карти до <main>
+createAndAppendCards(cardData, 3);
