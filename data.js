7980f4ddf636777c4f15eae385b5a359bded40cc
Bohdan Rudenko <b.r.rudenko@student.khai.edu>
2024-02-06 18:01:34 +0200

    feat: added 7 new cards

--- data.js
+++ data.js
@@ -6,7 +6,19 @@ var cardData = {
   "card-3":
     "The winds of change are whispering around you; stay attuned to the subtle shifts, as they may guide you towards unexpected transformations.",
   "card-4":
-    "The winds of change are whispering around you; stay attuned to the subtle shifts, as they may guide you towards unexpected transformations.",
+    "Trust your instincts as you navigate through a period of ambiguity; your intuition holds the power to illuminate the path ahead.",
+  "card-5":
+    "Be mindful of interpersonal dynamics, as conflicts may arise – seek understanding and compromise to maintain harmony.",
+  "card-6":
+    "The veil of mystery shrouds your personal relationships; embrace communication and vulnerability to strengthen the bonds that matter.",
+  "card-7":
+    "A creative spark is ready to ignite within you; channel your inspiration and let your imaginative endeavors flourish.",
+  "card-8":
+    "Guard your health and well-being, for unseen challenges may test your resilience – prioritize self-care and balance.",
+  "card-9":
+    "Unexpected opportunities may knock on your door; be open to serendipity and seize the chances that align with your true aspirations.",
+  "card-10":
+    "Your journey may encounter detours, but each diversion carries valuable lessons – embrace the twists and turns with resilience and adaptability.",
 };
 
 // Викликайте функцію з app.js, яка буде створювати та додавати карти до <main>
